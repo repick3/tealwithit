727faf7dd5f86facf1b71d93d267a4458b0d6e8a
Matt Chambers <repick3@gmail.com>
2016-09-11 01:24:25 -0500

    added links to specific pages

--- app/views.py
+++ app/views.py
@@ -8,6 +8,17 @@ import collections
 def hello_world():
     return render_template('/index.html')
 
+@app.route('/teal/<gfy_url>/')
+def get_specific_teal(gfy_url):
+    try:
+        post = models.check_for_post(gfy_url)
+    except Exception as e:
+        return (render_template('/404.html'), 404)
+    
+    return render_template('/post.html',
+        post=post
+    )
+
 @app.route('/random/')
 def random_teal():
     return(
